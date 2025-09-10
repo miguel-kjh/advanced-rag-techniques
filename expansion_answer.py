f5649418228cfeec43177341caee7071856c72ea
miguel-kjh <miguelangel.kjh@gmail.com>
2025-09-10 17:20:53 +0100

    Update .gitignore and requirements for ChromaDB integration and new dependencies

--- expansion_answer.py
+++ expansion_answer.py
@@ -1,6 +1,7 @@
+from typing import List
 from pypdf import PdfReader
 import os
-import openai
+from openai import OpenAI
 import chromadb
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from dotenv import load_dotenv
@@ -8,19 +9,44 @@ from langchain.text_splitter import (
     RecursiveCharacterTextSplitter,
     SentenceTransformersTokenTextSplitter,
 )
+import umap
+import matplotlib.pyplot as plt
 
-from helper_utils import word_wrap
+from helper_utils import project_embeddings, word_wrap
 from helper_utils import extract_text_from_pdf
 
-FOLDER = "data"
+FOLDER   = "data"
 PDF_FILE = "microsoft-annual-report.pdf"
 PDF_PATH = os.path.join(FOLDER, PDF_FILE)
+BD_PATH  = os.path.join(FOLDER, "chroma_db")
+COLLECTION_NAME = "annual_report"
 
-#main
+TOP_K = 5
+SEED  = 123
 
-def main():
-    load_dotenv()
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+def augment_query_generation(query: str, client: OpenAI, model: str = "gpt-4o-mini") -> str:
+    system_prompt = """
+    You are a helpful expert financial research assistant. 
+    Provide an example answer to the given question, that might be found in a document like an annual report.
+    """
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": query},
+        ],
+        temperature=0.7,
+        seed=SEED,
+    )
+    augmented_query = response.choices[0].message.content
+    return augmented_query
+
+def create_db(texts: List[str], client: chromadb.PersistentClient, embedding_function: SentenceTransformerEmbeddingFunction) -> chromadb.api.models.Collection:
+    collection = client.create_collection(name=COLLECTION_NAME, embedding_function=embedding_function)
+    collection.add(documents=texts, ids=[str(i) for i in range(len(texts))])
+    return collection
+
+def chunking_data() -> List[str]:
     reader = PdfReader(PDF_PATH)
     pdf_texts = [
         p.extract_text().strip() for p in reader.pages
@@ -44,9 +70,97 @@ def main():
 
     print(f"Number of character-based chunks: {len(character_split_texts)}")
     print(f"Number of token-based chunks: {len(token_split_texts)}")
+    return token_split_texts
 
+#main
+def main():
+    load_dotenv()
+    openai_key = os.getenv("OPENAI_API_KEY")
+    openai_client = OpenAI(api_key=openai_key)
+    client = chromadb.PersistentClient(path=BD_PATH)
+    existing = [c.name for c in client.list_collections()]
+    print(f"Existing collections: {existing}")
     embedding_function = SentenceTransformerEmbeddingFunction(device="cuda")
-    print(embedding_function([token_split_texts[0]]))
+
+    if COLLECTION_NAME in existing:
+        collection = client.get_collection(
+            name=COLLECTION_NAME,
+            embedding_function=embedding_function
+        )
+        print(f"Existing collection named {COLLECTION_NAME} found.")
+    else:
+        token_split_texts = chunking_data()
+        collection = create_db(token_split_texts, client, embedding_function)
+        print("Collection created and data added.")
+    
+
+    # count
+    print(f"Number of documents in the collection: {collection.count()}")
+
+    query = "What was the total profit for the year, and how does it compare to the previous year?"
+    hypothetical_answer  = augment_query_generation(query, openai_client)
+    # join query and augmented query
+    join_query = f"{query} {hypothetical_answer}"
+    results = collection.query(
+        query_texts=[join_query],
+        n_results=TOP_K,
+        include=["documents", "embeddings", "distances"]
+    )
+    
+    
+    embeddings = collection.get(include=["embeddings"])["embeddings"]
+    umap_transform = umap.UMAP(random_state=SEED, transform_seed=SEED).fit(embeddings)
+    projected_dataset_embeddings = project_embeddings(embeddings, umap_transform)
+
+    retrieved_embeddings = results["embeddings"][0]
+    original_query_embedding = embedding_function([query])
+    augmented_query_embedding = embedding_function([join_query])
+
+    projected_original_query_embedding = project_embeddings(
+        original_query_embedding, umap_transform
+    )
+    projected_augmented_query_embedding = project_embeddings(
+        augmented_query_embedding, umap_transform
+    )
+    projected_retrieved_embeddings = project_embeddings(
+        retrieved_embeddings, umap_transform
+    )
+
+    # Plot the projected query and retrieved documents in the embedding space
+    plt.figure()
+
+    plt.scatter(
+        projected_dataset_embeddings[:, 0],
+        projected_dataset_embeddings[:, 1],
+        s=10,
+        color="gray",
+    )
+    plt.scatter(
+        projected_retrieved_embeddings[:, 0],
+        projected_retrieved_embeddings[:, 1],
+        s=100,
+        facecolors="none",
+        edgecolors="g",
+    )
+    plt.scatter(
+        projected_original_query_embedding[:, 0],
+        projected_original_query_embedding[:, 1],
+        s=150,
+        marker="X",
+        color="r",
+    )
+    plt.scatter(
+        projected_augmented_query_embedding[:, 0],
+        projected_augmented_query_embedding[:, 1],
+        s=150,
+        marker="X",
+        color="orange",
+    )
+
+    plt.gca().set_aspect("equal", "datalim")
+    plt.title(f"{query}")
+    plt.axis("off")
+    plt.show()  # display the plot
 
 if __name__ == "__main__":
     main()
